6f9890bc5b0120e9e6697fd66d18a44b0d6506e9
Harsh Agrawal <harshagrawal180806@gmail.com>
2025-06-30 01:19:24 +0530

    Fix ESLint warnings and clean up unused variables

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import NewspaperContainer from './components/NewspaperContainer';
